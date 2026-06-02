7f0e08ac95d0afd50df5f725dfd8488afb7e0653
Roman <romabogush82@gmail.com>
2026-06-02 09:18:30 +0200

    Refine player movement

--- scenes/character/player/idle_state.gd
+++ scenes/character/player/idle_state.gd
@@ -1,24 +1,21 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
-
-var direction: Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	direction = GameInputEvents.movement_input()
 	
-	if direction == Vector2.UP:
+	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("idle_back")
-	elif direction == Vector2.DOWN:
+	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("idle_front")
-	elif direction == Vector2.LEFT:
+	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("idle_left")
-	elif direction == Vector2.RIGHT:
+	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("idle_right")
 	else:
 		animated_sprite_2d.play("idle_front")
@@ -35,4 +32,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
--- scenes/character/player/walk_state.gd
+++ scenes/character/player/walk_state.gd
@@ -1,6 +1,6 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var speed: int = 50
 
@@ -21,10 +21,14 @@ func _on_physics_process(_delta : float) -> void:
 		animated_sprite_2d.play("walk_right")
 	player.velocity = direction * speed
 	player.move_and_slide()
+	
+	if direction != Vector2.ZERO:
+		player.player_direction = direction
 
 
 func _on_next_transitions() -> void:
-	pass
+	if !GameInputEvents.is_movement_input():
+		transition.emit("idle")
 
 
 func _on_enter() -> void:
@@ -32,4 +36,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
