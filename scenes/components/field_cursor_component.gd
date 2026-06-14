5010a6eea5803f68c4b2d1d0c0e857ef2f55024c
Roman <romabogush82@gmail.com>
2026-06-14 16:49:01 +0200

    lvl 1

--- scenes/components/field_cursor_component.gd
+++ scenes/components/field_cursor_component.gd
@@ -16,8 +16,7 @@ var distance: float
 func _ready() -> void:
 	await get_tree().process_frame
 	player = get_tree().get_first_node_in_group("player")
-
-
+	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("remove_dirt"):
 		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
@@ -27,19 +26,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
 			get_cell_under_mouse()
 			add_tilled_soil_cell()
-
+			
 func get_cell_under_mouse() -> void:
 	mouse_position = grass_tilemap_layer.get_local_mouse_position()
 	cell_position = grass_tilemap_layer.local_to_map(mouse_position)
 	cell_source_id = grass_tilemap_layer.get_cell_source_id(cell_position)
 	local_cell_position = grass_tilemap_layer.map_to_local(cell_position)
 	distance = player.global_position.distance_to(local_cell_position)
-
-
+	
 func add_tilled_soil_cell() -> void:
+	print("distance: ", distance, " | cell_source_id: ", cell_source_id)
 	if distance < 20.0 && cell_source_id != -1:
 		tilled_soil_tilemap_layer.set_cells_terrain_connect([cell_position], terrain_set, terrain, true)
-
+		
 func remove_tilled_soil_cell() -> void:
 	if distance < 20.0:
 		tilled_soil_tilemap_layer.set_cells_terrain_connect([cell_position], 0, -1, true)
